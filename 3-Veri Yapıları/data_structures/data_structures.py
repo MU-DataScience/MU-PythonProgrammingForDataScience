e1364caede8283f893b4ac07ba7b46bf647124eb
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-17 10:35:53 +0300

    2-Sayılar

--- 3-Veri Yapıları/data_structures/data_structures.py
+++ 3-Veri Yapıları/data_structures/data_structures.py
@@ -57,3 +57,27 @@ x = {"python", "ml", "ds"}
 type(x)
 
 # Not: Liste, tuple, set ve dictionary veri yapıları aynı zamanda Python Collections (Arrays) olarak geçmektedir.
+
+###########################################
+# Sayılar (Numbers): int, float, complex
+###########################################
+
+a = 5
+b = 10.5
+
+a * 3
+a / 7
+a * b / 10
+a ** 2
+
+#########################
+# Tipleri Değiştirmek
+#########################
+
+int(b)
+float(a)
+
+int(a * b / 10)
+
+c = a * b / 10
+int(c)
