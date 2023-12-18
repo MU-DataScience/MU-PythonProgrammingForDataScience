eb93a9234f67f4d1be6a0f0ac25dd8d06ab01370
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-18 20:39:55 +0300

    6-Sözlük (Dictionary)

--- 3-Veri Yapıları/data_structures/data_structures.py
+++ 3-Veri Yapıları/data_structures/data_structures.py
@@ -250,3 +250,81 @@ notes.pop(0)
 ######################
 
 notes.insert(2, 99)
+
+###########################################
+# Sözlük (Dictionary)
+###########################################
+
+# Değiştirilebilir.
+# Sırasız. (3.7 sürümünden sonra sıralı.)
+# Kapsayıcı
+
+# key-value
+
+dictionary = {"REG": "Regression",
+              "LOG": "Logistic Regression",
+              "CART": "Classification and Reg"}
+
+dictionary["REG"]
+
+dictionary = {"REG": ["RMSE", 10],
+              "LOG": ["MSE", 20],
+              "CART": ["SSE", 30]}
+
+dictionary["REG"]
+
+dictionary = {"REG": 10,
+              "LOG": 20,
+              "CART": 30}
+
+dictionary["CART"][1]
+
+#######################
+# Key Sorgulama
+#######################
+
+"REG" in dictionary
+"YSA" in dictionary
+
+#######################
+# Key'e Göre Value'ya Erişmek
+#######################
+
+dictionary["REG"]
+dictionary.get("REG")
+
+#######################
+# Value Değiştirmek
+#######################
+
+dictionary["REG"] = ["YSA", 10]
+
+#######################
+# Tüm Key'lere Erişmek
+#######################
+
+dictionary.keys()
+
+#######################
+# Tüm Value'lara Erişmek
+#######################
+
+dictionary.values()
+
+#######################
+# Tüm Çiftleri Tuple Halinde Listeye Çevirme
+#######################
+
+dictionary.items()
+
+#######################
+# Key-Value Değerini Güncellemek
+#######################
+
+dictionary.update({"REG": 11})
+
+#######################
+# Yeni Key-Value Eklemek
+#######################
+
+dictionary.update({"RF": 10})
