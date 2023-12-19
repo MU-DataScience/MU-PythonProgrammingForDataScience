a379529eeeef7cc325143d0972719980d4d8ed24
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-19 20:44:33 +0300

    5-Ne Zaman Fonksiyon Yazılır?

--- 4-Fonksiyonlar/functions_conditions_loop_comprehensions.py/functions_conditions_loops_comprehensions.py
+++ 4-Fonksiyonlar/functions_conditions_loop_comprehensions.py/functions_conditions_loops_comprehensions.py
@@ -142,3 +142,22 @@ def say_hi(string="Merhaba"):
 
 say_hi()
 say_hi("mrb")
+
+########################
+# Ne Zaman Fonksiyon Yazma İhtiyacımız Olur?
+########################
+
+# varm, moisture, charge
+
+(56 + 15) / 80
+(17 + 45) / 70
+(52 + 45) / 80
+
+
+# DRY
+
+def calculate(varm, moisture, charge):
+    print((varm + moisture) / charge)
+
+
+calculate(98, 12, 78)
