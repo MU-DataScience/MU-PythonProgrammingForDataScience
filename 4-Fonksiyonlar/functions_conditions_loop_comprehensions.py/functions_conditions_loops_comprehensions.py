7969384cad063491d8cec442450084735e33caec
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-19 20:58:10 +0300

    7-Fonksiyon İçerisinden Fonksiyon Çağırmak

--- 4-Fonksiyonlar/functions_conditions_loop_comprehensions.py/functions_conditions_loops_comprehensions.py
+++ 4-Fonksiyonlar/functions_conditions_loop_comprehensions.py/functions_conditions_loops_comprehensions.py
@@ -198,3 +198,39 @@ calculate(98, 12, 78)
 type(calculate(98, 12, 78))
 
 varm, moisture, charge, output = calculate(98, 12, 78)
+
+
+#######################
+# Fonksiyon İçerisinden Fonksiyon Çağırmak
+#######################
+
+def calculate(varm, moisture, charge):
+    return int((varm + moisture) / charge)
+
+
+calculate(90, 12, 12) * 10
+
+
+def standardization(a, p):
+    return a * 10 / 100 * p * p
+
+
+standardization(45, 1)
+
+
+def all_calculation(varm, moisture, charge, p):
+    a = calculate(varm, moisture, charge)
+    b = standardization(a, p)
+    print(b * 10)
+
+
+all_calculation(1, 3, 5, 12)
+
+
+def all_calculation(varm, moisture, charge, a, p):
+    print(calculate(varm, moisture, charge))
+    b = standardization(a, p)
+    print(b * 10)
+
+
+all_calculation(1, 3, 5, 19, 12)
