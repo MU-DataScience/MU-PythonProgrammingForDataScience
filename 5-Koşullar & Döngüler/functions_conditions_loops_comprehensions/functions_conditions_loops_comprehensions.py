57f255ca16874c29dc79bb4c1bc9f9b92759c47e
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-20 20:19:29 +0300

    5-Break & While & Continue

--- 5-Koşullar & Döngüler/functions_conditions_loops_comprehensions/functions_conditions_loops_comprehensions.py
+++ 5-Koşullar & Döngüler/functions_conditions_loops_comprehensions/functions_conditions_loops_comprehensions.py
@@ -165,5 +165,29 @@ def alternating(string):
             new_string += string[string_index].lower()
     print(new_string)
 
+
 alternating("miuul")
 # endregion
+
+#######################
+# break & continue & while
+#######################
+
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+for salary in salaries:
+    if salary == 3000:
+        break
+    print(salary)
+
+for salary in salaries:
+    if salary == 3000:
+        continue
+    print(salary)
+
+# while
+
+number = 1
+while number < 5:
+    print(number)
+    number += 1
