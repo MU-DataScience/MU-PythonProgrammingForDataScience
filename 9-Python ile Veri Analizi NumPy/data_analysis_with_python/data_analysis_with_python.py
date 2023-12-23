62bcc768e5605fbee58648503ad087ebea705277
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-23 20:04:25 +0300

    2-NumPy Giriş

--- 9-Python ile Veri Analizi NumPy/data_analysis_with_python/data_analysis_with_python.py
+++ 9-Python ile Veri Analizi NumPy/data_analysis_with_python/data_analysis_with_python.py
@@ -5,3 +5,35 @@
 # - Pandas
 # - Veri Görselleştirme: Matplotlib & Seaborn
 # - Gelişmiş Fonksiyonel Keşifçi Veri Analizi (Advanced Functional Exploratory Data Analysis)
+
+##############################################
+# NUMPY
+##############################################
+
+# Neden NumPy? (Why Numpy?)
+# NumPy Array'i Oluşturmak (Creating Numpy Arrays)
+# Numpy Array Özellikleri (Attributes of Numpy Arrays)
+# Yeniden Şekillendirme (Reshaping)
+# Index Seçimi (Index Selection)
+# Slicing
+# Fancy Index
+# Numpy'da Koşullu İşlemler (Conditions on Numpy)
+# Matematiksel İşlemler (Mathematical Operations)
+
+################################
+# Neden NumPy?
+################################
+
+import numpy as np
+
+a = [1, 2, 3, 4]
+b = [2, 3, 4, 5]
+
+ab = []
+
+for i in range(0, len(a)):
+    ab.append(a[i] * b[i])
+
+a = np.array([1, 2, 3, 4])
+b = np.array([2, 3, 4, 5])
+a * b
