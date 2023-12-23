14caf4341a0a8cd8c1d713f3c54fa7352add4b8e
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-23 21:17:56 +0300

    8-NumPy Koşullu İşlemler

--- 9-Python ile Veri Analizi NumPy/data_analysis_with_python/data_analysis_with_python.py
+++ 9-Python ile Veri Analizi NumPy/data_analysis_with_python/data_analysis_with_python.py
@@ -111,3 +111,34 @@ v[4]
 catch = [1, 2, 3]
 
 v[catch]
+
+################################
+# Numpy'da Koşullu İşlemler (Conditions on Numpy)
+################################
+import numpy as np
+
+v = np.array([1, 2, 3, 4, 5])
+
+####################
+# Klasik Döngü İle
+####################
+
+ab = []
+
+for i in v:
+    print(i)
+
+for i in v:
+    if i < 3:
+        ab.append(i)
+
+####################
+# Numpy İle
+####################
+v < 3
+
+v[v < 3]
+v[v > 3]
+v[v != 3]
+v[v == 3]
+v[v >= 3]
