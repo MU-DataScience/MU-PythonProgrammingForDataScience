f10214034dddfc54296ca4b176c758081ac5df32
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-23 11:54:13 +0300

    3-Uygulama - Mülakat Sorusu Dict Comprehensions

--- 8-Comprehensions/functions_conditions_loops_comprehensions/functions_conditions_loops_comprehensions.py
+++ 8-Comprehensions/functions_conditions_loops_comprehensions/functions_conditions_loops_comprehensions.py
@@ -58,3 +58,19 @@ dictionary.items()
 {k.upper(): v for (k, v) in dictionary.items()}
 
 {k.upper(): v * 2 for (k, v) in dictionary.items()}
+
+#######################
+# Uygulama - Mülakat Sorusu
+#######################
+
+# Amaç: çift sayıların karesi alınarak bir sözlüğe eklenmek istemektedir.
+# key'ler orjinal değerler value'lar ise değiştirilmiş değerler olacak.
+
+numbers = range(10)
+new_dict = {}
+
+for n in numbers:
+    if n % 2 == 0:
+        new_dict[n] = n ** 2
+
+{n: n ** 2 for n in numbers if n % 2 == 0}
