aaf288bfdb4c9e98b367f28d028b2f2eafe27431
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-23 20:07:21 +0300

    3-NumPy Array'i Oluşturma

--- 9-Python ile Veri Analizi NumPy/data_analysis_with_python/data_analysis_with_python.py
+++ 9-Python ile Veri Analizi NumPy/data_analysis_with_python/data_analysis_with_python.py
@@ -37,3 +37,14 @@ for i in range(0, len(a)):
 a = np.array([1, 2, 3, 4])
 b = np.array([2, 3, 4, 5])
 a * b
+
+################################
+# NumPy Array'i Oluşturmak (Creating Numpy Arrays)
+################################
+import numpy as np
+
+np.array([1, 2, 3, 4, 5])
+type(np.array([1, 2, 3, 4, 5]))
+np.zeros(10, dtype=int)
+np.random.randint(0, 10, size=10)
+np.random.normal(10, 4, (3, 4))
