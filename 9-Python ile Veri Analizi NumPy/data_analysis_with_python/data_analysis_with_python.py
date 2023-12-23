7608baafa1ea9fd5193c1828a8ecd49e6cae55a7
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-23 20:15:05 +0300

    5-Reshaping

--- 9-Python ile Veri Analizi NumPy/data_analysis_with_python/data_analysis_with_python.py
+++ 9-Python ile Veri Analizi NumPy/data_analysis_with_python/data_analysis_with_python.py
@@ -64,3 +64,14 @@ a.ndim
 a.shape
 a.size
 a.dtype
+
+################################
+# Yeniden Şekillendirme (Reshaping)
+################################
+import numpy as np
+
+np.random.randint(1, 10, size=9)
+np.random.randint(1, 10, size=9).reshape(3, 3)
+
+ar = np.random.randint(1, 10, size=9)
+ar.reshape(3, 3)
