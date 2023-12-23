f71523c4bcbe8e01c9b165f16110147211066a8b
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-23 19:57:54 +0300

    1-Python ile Veri Analizine Giriş

---
+++ 9-Python ile Veri Analizi NumPy/data_analysis_with_python/data_analysis_with_python.py
@@ -0,0 +1,7 @@
+#############################################################
+# PYTHON İLE VERİ ANALİZİ (DATA ANALYSIS WITH PYTHON)
+#############################################################
+# - NumPy
+# - Pandas
+# - Veri Görselleştirme: Matplotlib & Seaborn
+# - Gelişmiş Fonksiyonel Keşifçi Veri Analizi (Advanced Functional Exploratory Data Analysis)
