4f7cc836a65ab96a6d5d1f81681c860785a6eb05
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-24 10:10:26 +0300

    5-Pandas Seçim İşlemleri

--- 10-Python ile Veri Analizi Pandas/data_analysis_with_python/data_analysis_with_python.py
+++ 10-Python ile Veri Analizi Pandas/data_analysis_with_python/data_analysis_with_python.py
@@ -52,3 +52,49 @@ df.isnull().values.any()
 df.isnull().sum()
 df["sex"].head()
 df["sex"].value_counts()
+
+#######################################
+# Pandas'ta Seçim İşlemleri (Selection in Pandas)
+#######################################
+import pandas as pd
+import seaborn as sns
+
+df = sns.load_dataset("titanic")
+df.head()
+
+df.index
+df[0:13]
+df.drop(0, axis=0).head()
+
+delete_indexes = [1, 3, 5, 7]
+df.drop(delete_indexes, axis=0).head(10)
+
+# df = df.drop(delete_indexes, axis=0)
+# df.drop(delete_indexes, axis=0, inplace=True)
+
+
+#########################
+# Değişkeni Index'e Çevirmek
+#########################
+df["age"].head()
+df.age.head()
+
+df.index = df["age"]
+
+df.drop("age", axis=1).head()
+
+df.drop("age", axis=1, inplace=True)
+df.head()
+
+#########################
+# Index'i Değişkene Çevirmek
+#########################
+
+df.index
+
+df["age"] = df.index
+df.head()
+df.drop("age", axis=1, inplace=True)
+
+df.reset_index().head()
+df = df.reset_index()
