a73f0bb0c2bb81b73ebeed2fccb03a4a118c2b4a
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-24 10:02:56 +0300

    4-Veriye Hızlı Bakış

--- 10-Python ile Veri Analizi Pandas/data_analysis_with_python/data_analysis_with_python.py
+++ 10-Python ile Veri Analizi Pandas/data_analysis_with_python/data_analysis_with_python.py
@@ -33,3 +33,22 @@ import pandas as pd
 df = pd.read_csv("datasets/advertising.csv")
 df.head()
 # pandas cheatsheet
+
+#######################################
+# Veriye Hızlı Bakış (Quick Look at Data)
+#######################################
+import pandas as pd
+import seaborn as sns
+
+df = sns.load_dataset("titanic")
+df.head()
+df.tail()
+df.shape
+df.info()
+df.columns
+df.index
+df.describe().T
+df.isnull().values.any()
+df.isnull().sum()
+df["sex"].head()
+df["sex"].value_counts()
