cb6c17f0fe5da7ddf7068c54003a3f5e8edb67d0
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-27 19:07:04 +0300

    1-Kategorik Değişken Görselleştirme

---
+++ 13-Python ile Veri Analizi Veri Görselleştirme/DataAnalysisWithPython/data_analysis_with_python.py
@@ -0,0 +1,27 @@
+#############################################
+# VERİ GÖRSELLEŞTİRME: MATPLOTLIB & SEABORN
+#############################################
+
+#############################################
+# MATPLOTLIB
+#############################################
+
+# Kategorik değişken: sütun grafik. Seaborn -> countplot, Matplotlib -> bar
+# Sayısal değişken: hist, boxplot
+
+# Veri görselleştirme teknikleriyle elde edilmek istenen şey elimizdeki değişkenin dağılımıdır/yapısıdır.
+
+#############################################
+# Kategorik Değişken Görselleştirme
+#############################################,
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["sex"].value_counts().plot(kind="bar")
+plt.show()
