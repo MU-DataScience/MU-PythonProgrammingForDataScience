3b17e2543c20f6ed08c296dbf22399f3fc94ce91
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-27 22:13:59 +0300

    4-Seaborn ile Veri Görselleştirme

--- 13-Python ile Veri Analizi Veri Görselleştirme/DataAnalysisWithPython/data_analysis_with_python.py
+++ 13-Python ile Veri Analizi Veri Görselleştirme/DataAnalysisWithPython/data_analysis_with_python.py
@@ -172,3 +172,30 @@ plt.title("3")
 plt.plot(x, y)
 
 plt.show()
+
+##############################################################
+# SEABORN
+##############################################################
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+
+df = sns.load_dataset("tips")
+df.head()
+
+df["sex"].value_counts()
+sns.countplot(x=df["sex"], data=df)
+plt.show()
+
+df["sex"].value_counts().plot(kind="bar")
+plt.show()
+
+###################################################
+# Sayısal Değişken Görselleştirme
+###################################################
+
+sns.boxplot(x=df["total_bill"])
+plt.show()
+
+df["total_bill"].hist()
+plt.show()
