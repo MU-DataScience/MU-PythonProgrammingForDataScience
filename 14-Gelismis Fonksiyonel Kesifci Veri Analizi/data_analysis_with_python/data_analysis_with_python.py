ed59b903c25f487fa730be1402c375c582204e67
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-12-29 23:21:11 +0300

    6-Hedef Değişken Analizi

--- 14-Gelismis Fonksiyonel Kesifci Veri Analizi/data_analysis_with_python/data_analysis_with_python.py
+++ 14-Gelismis Fonksiyonel Kesifci Veri Analizi/data_analysis_with_python/data_analysis_with_python.py
@@ -310,5 +310,122 @@ def cat_summary(dataframe, col_name, plot=False):
         sns.countplot(x=dataframe[col_name], data=dataframe)
         plt.show(block=True)
 
+
 for col in cat_cols:
-    cat_summary(df, col, plot=True)s
+    cat_summary(df, col, plot=True)
+
+#################################################
+# 4. Hedef Değişken Analizi (Analysis of Target Variable)
+#################################################
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+for col in df.columns:
+    if df[col].dtypes == "bool":
+        df[col] = df[col].astype("int64")
+
+
+def cat_summary(dataframe, col_name):
+    print(pd.DataFrame({col_name: dataframe[col_name].value_counts(),
+                        "Ratio": 100 * dataframe[col_name].value_counts() / len(dataframe)}))
+    print("#####################################")
+
+
+def grab_col_names(dataframe, cat_th=10, car_th=30):
+    """
+    Veri setindeki kategorik, numerik ve kategorik fakat kardinal değişkenlerin isimlerini verir.
+
+    Parameters
+    ----------
+    dataframe: dataframe
+        değişken isimleri alınmak istenen dataframe'dir.
+    cat_th: int, float
+        numerik fakat kategorik olan değişkenler için sınıf eşik değeri
+    car_th: int, float
+        kategorik fakat kardinal olan değişkenler için sınıf eşik değeri
+
+    Returns
+    -------
+    cat_cols: list
+        Kategorik değişken listesi
+    num_cols: list
+        Numerik değişken listesi
+    cat_but_car: list
+        Kategorik görünümlü kardinal değişken listesi
+
+    Notes
+    -------
+    cat_cols + num_cols + cat_but_car = toplam değişken sayısı
+    num_but_cat cat_cols'un içerisinde.
+
+    """
+    cat_cols = [col for col in dataframe.columns if str(dataframe[col].dtypes) in ["category", "object", "bool"]]
+    num_but_cat = [col for col in dataframe.columns if
+                   dataframe[col].nunique() < cat_th and dataframe[col].dtypes in ["int64", "float64"]]
+    cat_but_car = [col for col in dataframe.columns if
+                   dataframe[col].nunique() > car_th and str(dataframe[col].dtypes) in ["category", "object"]]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes in ["int64", "float64"]]
+    num_cols = [col for col in num_cols if col not in cat_cols]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f"cat_cols: {len(cat_cols)}")
+    print(f"num_cols: {len(num_cols)}")
+    print(f"cat_but_car: {len(cat_but_car)}")
+    print(f"num_but_cat: {len(num_but_cat)}")
+
+    return cat_cols, num_cols, cat_but_car
+
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+df.head()
+
+df["survived"].value_counts()
+cat_summary(df, "survived")
+
+#######################################
+# Hedef Değişkenin Kategorik Değişkenler ile Analizi
+#######################################
+
+df.groupby("sex")["survived"].mean()
+
+
+def target_summary_with_cat(dataframe, target, categorical_col):
+    print(pd.DataFrame({"TARGET_MEAN": dataframe.groupby(categorical_col)[target].mean()}))
+
+
+target_summary_with_cat(df, "survived", "sex")
+target_summary_with_cat(df, "survived", "pclass")
+
+for col in cat_cols:
+    target_summary_with_cat(df, "survived", col)
+
+#######################################
+# Hedef Değişkenin Sayısal Değişkenler ile Analizi
+#######################################
+
+df.groupby("survived")["age"].mean()
+
+df.groupby("survived").agg({"age": "mean"})
+
+
+def target_summary_with_num(dataframe, target, numerical_col):
+    print(dataframe.groupby(target).agg({numerical_col: "mean"}))
+
+
+target_summary_with_num(df, "survived", "age")
+
+for col in num_cols:
+    target_summary_with_num(df, "survived", col)
