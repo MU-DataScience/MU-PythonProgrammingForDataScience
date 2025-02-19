1353330b7ea029e5cf8025655c31dff962614c74
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-02-19 21:16:16 +0300

    feat:Initialize variables and implement basic number operations

---
+++ V2/pythonProject/setting_up_working_environment.py
@@ -0,0 +1,31 @@
+#######################################################
+# Sayılar (Numbers) ve Karakter Dizileri (Strings)
+#######################################################
+print("Hello World")
+print("Hello AI Era")
+
+print(9)
+9.2
+
+type(9)
+type(9.2)
+
+type("Mrb")
+
+#######################################################
+# Atamalar ve Değişkenler (Assignments & Variables)
+#######################################################
+
+a = 9
+a
+
+b = "hello ai era"
+b
+
+c = 10
+
+a * c
+
+a * 10
+
+d = a - c
