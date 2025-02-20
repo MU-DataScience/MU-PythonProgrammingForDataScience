155aeb518de58551dc7151a34d86dc552378cf43
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-02-20 23:43:10 +0300

    feat: Introduction to Data Structures and Quick Summary added
    - Explained the fundamental concepts of data structures.
    - Covered basic structures like arrays, linked lists, stacks, and queues.

---
+++ V2/pythonProject/2-DataStructures/data_structures.py
@@ -0,0 +1,58 @@
+##########################################################################
+# VERİ YAPILARI (DATA STRUCTURES)
+##########################################################################
+# - Veri Yapılarına Giriş ve Hızlı Özet
+# - Sayılar(Numbers): int, float, complex
+# - Karakter Dizileri(Strings) : str
+# - Boolean(TRUE-FALSE): bool
+# - Liste(List)
+# - Sözlük(Dictionary)
+# - Demet(Tuple)
+# - Set
+
+###################################################
+# Veri Yapılarına Giriş ve Hızlı Özet
+###################################################
+
+# Sayılar: integer
+x = 46
+type(x)
+
+# Sayılar: float
+x = 10.3
+type(x)
+
+# Sayılar: complex
+x = 2j + 1
+type(x)
+
+# String
+x = "Hello ai era"
+type(x)
+
+# Boolean
+True
+False
+type(True)
+5 == 4
+3 == 2
+1 == 1
+type(3 == 2)
+
+# Liste
+x = ["btc", "eth", "xrp"]
+type(x)
+
+# Sözlük (dictionary)
+x = {"name": "Peter", "Age": 36}
+type(x)
+
+# Tuple
+x = ("python", "ml", "ds")
+type(x)
+
+# Set
+x = {"python", "ml", "ds"}
+type(x)
+
+# Not: List, tuple, set ve dictionary veri yapıları aynı zamanda python collections (Arrays) olaarak geçmektedir.
