8e1435f3d03eaab105a1ac81ec40a16e724a7a9b
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-02-21 23:25:17 +0300

    feat: Added dictionary utilities
    - Implemented basic dictionary operations (add, update, delete keys)
    - Added methods for key and value extraction (keys, values, items)

--- V2/pythonProject/2-DataStructures/data_structures.py
+++ V2/pythonProject/2-DataStructures/data_structures.py
@@ -246,3 +246,81 @@ notes.pop(0)
 
 notes.insert(2, 99)
 
+###################################################
+# Sözlük (Dictionary)
+###################################################
+
+# Depiştirlebilir.
+# Sırasız. (3.7 sonrası sıralı.)
+# Kapsayıcı.
+
+# key-value
+
+dictionary = {"REG": "Regression",
+              "LOG": "Logistic Regression",
+              "CART": "Classification and Reg"}
+
+dictionary["REG"]
+
+dictionary = {"REG": ["RMSE", 10],
+              "LOG": ["MSE", 20],
+              "CART": ["SSE", 30]}
+
+dictionary["REG"]
+dictionary["CART"][1]
+
+dictionary = {"REG": 10,
+              "LOG": 20,
+              "CART": 30}
+
+dictionary["REG"]
+
+#######################
+# Key Sorgulama
+#######################
+
+"REG" in dictionary
+"YSA" in dictionary
+
+#######################
+# Key'e Göre Value'ya Erişmek
+#######################
+
+dictionary["REG"]
+dictionary.get("REG")
+
+#######################
+# Value Değiştirmek
+#######################
+
+dictionary["REG"] = ["YSA", 10]
+
+#######################
+# Tüm Key'lere Erişmek
+#######################
+
+dictionary.keys()
+
+#######################
+# Tüm Value'lara Erişmek
+#######################
+
+dictionary.values()
+
+#######################
+# Tüm Çiftleri Tuple Halinde Listeye Çevirme
+#######################
+
+dictionary.items()
+
+#######################
+# Key-Value Değerini Güncellemek
+#######################
+
+dictionary.update({"REG": 11})
+
+#######################
+# Yeni Key-Value Eklemek
+#######################
+
+dictionary.update({"RF": 10})
