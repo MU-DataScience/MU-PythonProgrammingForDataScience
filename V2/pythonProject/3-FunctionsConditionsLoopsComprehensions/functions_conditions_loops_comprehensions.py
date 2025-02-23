49fed5cdb945fbeb7bbc30141db270babb2bf258
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-02-23 22:39:08 +0300

    feat: Added docstring practices for function documentation
    - Included docstring examples explaining purpose, parameters, return values, and usage
    - Emphasized in-function documentation with """triple quotes""" syntax
    - Demonstrated PEP 257-compliant docstring writing
    - Integrated examples with help() function and auto-doc tools

--- V2/pythonProject/3-FunctionsConditionsLoopsComprehensions/functions_conditions_loops_comprehensions.py
+++ V2/pythonProject/3-FunctionsConditionsLoopsComprehensions/functions_conditions_loops_comprehensions.py
@@ -45,3 +45,35 @@ def summer(arg1, arg2):
 summer(7, 8)
 summer(8, 7)
 summer(arg2=8, arg1=7)
+
+
+#########################
+# Docstring
+#########################
+
+def summer(arg1, arg2):
+    print(arg1 + arg2)
+
+
+def summer(arg1, arg2):
+    """
+    Sum of two numbers
+    Args:
+        arg1: int,float
+        arg2: int,float
+
+    Returns:
+            int,float
+
+    Examples:
+
+    Notes:
+        
+
+    """
+    print(arg1 + arg2)
+
+
+summer(1, 3)
+
+help(summer)
