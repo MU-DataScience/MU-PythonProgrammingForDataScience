d69471329389ac9be3327ee06f16b2bea8e97951
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-02-23 22:30:58 +0300

    feat: Added Python functions introduction and literacy basics
    - Covered function definition (def syntax), parameters, and return statements
    - Added example applications and practical exercises for function literacy
    - Emphasized naming conventions, readability, and reusability best practices
    - Developed foundational skills for creating functions and interpreting existing code

---
+++ V2/pythonProject/3-FunctionsConditionsLoopsComprehensions/functions_conditions_loops_comprehensions.py
@@ -0,0 +1,47 @@
+#########################################################################
+# FONKSİYONLAR, KOŞULLAR, DÖNGÜLER, COMPREHENSIONS
+#########################################################################
+# - Fonksiyonlar(Functions)
+# - Koşullar(Conditions)
+# - Döngüler(Loops)
+# - Comprehensions
+
+##########################################
+# FONKSİYONLAR(FUNCTIONS)
+##########################################
+
+#########################
+# Fonksiyon Okuryazarlığı
+#########################
+# Parametre fonksiyon tanımlanması esnasıdan ifade edilen değişkenlerdir. Argüman ise bu fonksiyonlar çağırıldığında bu parametre değerlerine karşılık girilen değerlerdir.
+
+
+print("a")
+# ?print
+
+print("a", "b")
+print("a", "b", sep="__")
+
+help(print)
+
+
+#########################
+# Fonksiyon Tanımlama
+#########################
+
+def calculate(x):
+    print(x * 2)
+
+
+calculate(5)
+
+
+# İki argümanlı/parametreli bir fonksiyon tanımlayalım
+
+def summer(arg1, arg2):
+    print(arg1 + arg2)
+
+
+summer(7, 8)
+summer(8, 7)
+summer(arg2=8, arg1=7)
