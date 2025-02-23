dadf6ef68504a6d1aeb973237eb0813cc5a4f9cb
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-02-23 22:51:09 +0300

    feat: Expanded function body/statement sections

--- V2/pythonProject/3-FunctionsConditionsLoopsComprehensions/functions_conditions_loops_comprehensions.py
+++ V2/pythonProject/3-FunctionsConditionsLoopsComprehensions/functions_conditions_loops_comprehensions.py
@@ -68,7 +68,7 @@ def summer(arg1, arg2):
     Examples:
 
     Notes:
-        
+
 
     """
     print(arg1 + arg2)
@@ -77,3 +77,42 @@ def summer(arg1, arg2):
 summer(1, 3)
 
 help(summer)
+
+
+##########################################
+# Fonksiyonların Statement/Body Bölümü
+##########################################
+
+# def function_name(parameters/arguments):
+#               statements(function body)
+
+def say_hi(string):
+    print(string)
+    print("Hi")
+    print("Hello")
+
+
+say_hi("miuul")
+
+
+def multiplication(a, b):
+    c = a * b
+    print(c)
+
+
+multiplication(10, 9)
+
+# girilen değerleri bir liste içinde saklayacak fonksiyon
+
+list_store = []
+
+
+def add_element(a, b):
+    c = a * b
+    list_store.append(c)
+    print(list_store)
+
+
+add_element(1, 8)
+add_element(18, 8)
+add_element(180,10)
