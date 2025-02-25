d21c5bb9cfea65f1139be61971400d4b6acd115b
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-02-25 22:20:26 +0300

    feat: Added explanation and examples for Python for loops.

--- V2/pythonProject/4-ConditionsAndLoops/conditions_loops.py
+++ V2/pythonProject/4-ConditionsAndLoops/conditions_loops.py
@@ -59,3 +59,50 @@ def number_check(number):
 number_check(10)
 number_check(12)
 number_check(6)
+
+###################################################
+# DÖNGÜLER (LOOPS)
+###################################################
+
+# for loop
+
+students = ["John", "Mark", "Vanessa", "Mariam"]
+students[0]
+students[1]
+students[2]
+
+for student in students:
+    print(student)
+
+for student in students:
+    print(student.upper())
+
+salaries = [1000, 2000, 3000, 4000, 5000]
+
+for salary in salaries:
+    print(salary)
+
+for salary in salaries:
+    print(int(salary * 20 / 100 + salary))
+
+
+def new_salary(salary, rate):
+    return int(salary * rate / 100 + salary)
+
+
+new_salary(1500, 10)
+new_salary(2000, 20)
+
+for salary in salaries:
+    print(new_salary(salary, 10))
+
+salaries2 = [10700, 25000, 30400, 40300, 50200]
+
+for salary in salaries2:
+    print(new_salary(salary, 15))
+
+for salary in salaries:
+    if salary >= 3000:
+        print(new_salary(salary, 10))
+    else:
+        print(new_salary(salary, 20))
