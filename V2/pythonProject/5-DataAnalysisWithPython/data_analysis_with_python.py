53dd2bd8f5c152fcf078d8adef8ad767689b6ff8
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-02-28 23:21:35 +0300

    feat:Added section on reshaping NumPy arrays

--- V2/pythonProject/5-DataAnalysisWithPython/data_analysis_with_python.py
+++ V2/pythonProject/5-DataAnalysisWithPython/data_analysis_with_python.py
@@ -65,3 +65,16 @@ a.ndim
 a.shape
 a.size
 a.dtype
+
+###############################
+# Yeniden Şekillendirme(Reshaping)
+###############################
+import numpy as np
+
+np.random.randint(1, 10, size=9)
+np.random.randint(1, 10, size=9).reshape(3, 3)
+
+ar = np.random.randint(1, 10, size=9)
+ar.reshape(3,3)
+
+np.random.randint(1, 10, size=10).reshape(3, 3)
