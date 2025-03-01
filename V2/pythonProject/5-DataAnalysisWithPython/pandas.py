bb57e842c7ea5513e1b35de15059d18523b588ff
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-03-01 22:41:54 +0300

    feat: Enhance data analysis with aggregation and grouping operations

--- V2/pythonProject/5-DataAnalysisWithPython/pandas.py
+++ V2/pythonProject/5-DataAnalysisWithPython/pandas.py
@@ -177,7 +177,52 @@ df.loc[(df["age"] > 50) & (df["sex"] == "male"), ["age", "class"]].head()
 
 df["embark_town"].value_counts()
 
-df_new = df.loc[(df["age"] > 50) & (df["sex"] == "male") & ((df["embark_town"] == "Cherbourg") | (df["embark_town"] == "Southampton") ),
-["age", "class","embark_town"]]
+df_new = df.loc[(df["age"] > 50) & (df["sex"] == "male") & (
+        (df["embark_town"] == "Cherbourg") | (df["embark_town"] == "Southampton")),
+["age", "class", "embark_town"]]
 
 df_new["embark_town"].value_counts()
+
+################################
+# Toplulaştırma & Gruplama (Aggregation & Grouping)
+################################
+
+# - count()
+# - first()
+# - last()
+# - mean()
+# - median()
+# - min()
+# - max()
+# - std()
+# - var()
+# - sum()
+# - pivot table
+
+import pandas as pd
+import seaborn as sns
+
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["age"].mean()
+
+df.groupby("sex")["age"].mean()
+
+df.groupby("sex").agg({"age": "mean"})
+df.groupby("sex").agg({"age": ["mean", "sum"]})
+
+df.groupby("sex").agg({"age": ["mean", "sum"],
+                       "survived": "mean"})
+
+df.groupby(["sex", "embark_town"]).agg({"age": "mean",
+                                        "survived": "mean"})
+
+df.groupby(["sex", "embark_town", "class"]).agg({"age": "mean",
+                                                 "survived": "mean"})
+
+df.groupby(["sex", "embark_town", "class"]).agg({
+    "age": "mean",
+    "survived": "mean",
+    "sex": "count"})
