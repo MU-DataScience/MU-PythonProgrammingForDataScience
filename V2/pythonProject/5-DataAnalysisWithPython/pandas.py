71e68173d9ecdd3559a1471a2678e93838926716
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-03-01 21:08:55 +0300

    feat: Added data exploration utilities for initial DataFrame analysis

--- V2/pythonProject/5-DataAnalysisWithPython/pandas.py
+++ V2/pythonProject/5-DataAnalysisWithPython/pandas.py
@@ -34,3 +34,22 @@ import pandas as pd
 df = pd.read_csv("5-DataAnalysisWithPython/datasets/advertising.csv")
 df.head()
 # pandas cheatsheet
+
+################################
+# Veriye Hızlı Bakış (Quick Look at Data)
+################################
+import pandas as pd
+import seaborn as sns
+
+df = sns.load_dataset("titanic")
+df.head()
+df.tail()
+df.shape
+df.info()
+df.columns
+df.index
+df.describe().T
+df.isnull().values.any()
+df.isnull().sum()
+df["sex"].head()
+df["sex"].value_counts()
