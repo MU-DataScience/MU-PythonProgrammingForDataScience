12a888e3170a5243ef958dfa242683fcd767a69d
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-03-02 13:04:35 +0300

    feat:Added apply() with lambda functions for flexible data transformations

--- V2/pythonProject/5-DataAnalysisWithPython/pandas.py
+++ V2/pythonProject/5-DataAnalysisWithPython/pandas.py
@@ -249,8 +249,50 @@ df.head()
 df["new_age"] = pd.cut(df["age"], [0, 10, 18, 25, 40, 90])
 df.head()
 
-df.pivot_table("survived","sex", ["new_age"])
-df.pivot_table("survived","sex", ["new_age","class"])
+df.pivot_table("survived", "sex", ["new_age"])
+df.pivot_table("survived", "sex", ["new_age", "class"])
 
 # pd.set_option('display.width',500)
-pd.set_option('display.width',500)
+pd.set_option('display.width', 500)
+
+################################
+# Pivot Table
+################################
+import pandas as pd
+import seaborn as sns
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["age2"] = df["age"] * 2
+df["age3"] = df["age"] * 5
+
+(df["age"] / 10).head()
+(df["age2"] / 10).head()
+(df["age3"] / 10).head()
+
+for col in df.columns:
+    if "age" in col:
+        df[col] = df[col] / 10
+
+df.head()
+
+df[["age", "age2", "age3"]].apply(lambda x: x / 10).head()
+
+df.loc[:, df.columns.str.contains("age")].apply(lambda x: x / 10).head()
+
+df.loc[:, df.columns.str.contains("age")].apply(lambda x: (x - x.mean()) / x.std()).head()
+
+
+def standart_scaler(col_name):
+    return (col_name - col_name.mean()) / col_name.std()
+
+
+df.loc[:, df.columns.str.contains("age")].apply(standart_scaler).head()
+
+df.loc[:, ["age", "age2", "age3"]] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler)
+df.loc[:, df.columns.str.contains("age")] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler)
+
+df.head()
