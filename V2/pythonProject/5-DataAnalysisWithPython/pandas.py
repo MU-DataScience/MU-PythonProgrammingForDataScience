e07f19b35f4087bc26630ddf109a6fe7a4e31fea
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-03-02 12:42:27 +0300

    feat:Implemented pivot table functionality using pandas for data summarization

--- V2/pythonProject/5-DataAnalysisWithPython/pandas.py
+++ V2/pythonProject/5-DataAnalysisWithPython/pandas.py
@@ -225,4 +225,32 @@ df.groupby(["sex", "embark_town", "class"]).agg({"age": "mean",
 df.groupby(["sex", "embark_town", "class"]).agg({
     "age": "mean",
     "survived": "mean",
+
     "sex": "count"})
+
+################################
+# Pivot Table
+################################
+import pandas as pd
+import seaborn as sns
+
+pd.set_option('display.max_columns', None)
+df = sns.load_dataset("titanic")
+df.head()
+
+df.pivot_table("survived", "sex", "embarked")
+
+df.pivot_table("survived", "sex", "embarked", aggfunc="std")
+
+df.pivot_table("survived", "sex", ["embarked", "class"], aggfunc="std")
+
+df.head()
+
+df["new_age"] = pd.cut(df["age"], [0, 10, 18, 25, 40, 90])
+df.head()
+
+df.pivot_table("survived","sex", ["new_age"])
+df.pivot_table("survived","sex", ["new_age","class"])
+
+# pd.set_option('display.width',500)
+pd.set_option('display.width',500)
