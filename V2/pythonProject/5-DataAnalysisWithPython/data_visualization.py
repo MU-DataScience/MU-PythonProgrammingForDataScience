0e90e62d9f27dc902a627d6fd11dcd6479ab24d2
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-03-02 15:35:23 +0300

    feat: Added categorical variable visualization utilities (bar plots, count plots)

---
+++ V2/pythonProject/5-DataAnalysisWithPython/data_visualization.py
@@ -0,0 +1,25 @@
+####################################################
+# VERİ GÖRSELLEŞTİRME: MATPLOTLIB & SEABORN
+####################################################
+
+####################################################
+# MATPLOTLIB
+####################################################
+
+# Kategorik değişken: sütun grafik. pasta grafik. counplot bar
+# Sayısal değişken: hist, boxplot
+
+####################################################
+# Kategorik Değişken Görselleştirme
+####################################################
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+
+df["sex"].value_counts().plot(kind="bar")
+plt.show()
