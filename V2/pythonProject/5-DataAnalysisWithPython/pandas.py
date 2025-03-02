7f0e0ecc2e19a9281e76790f5acbb8fc00197b0d
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-03-02 14:43:23 +0300

    feat:Implemented merge operations (concat, merge) for combining datasets

--- V2/pythonProject/5-DataAnalysisWithPython/pandas.py
+++ V2/pythonProject/5-DataAnalysisWithPython/pandas.py
@@ -296,3 +296,38 @@ df.loc[:, ["age", "age2", "age3"]] = df.loc[:, df.columns.str.contains("age")].a
 df.loc[:, df.columns.str.contains("age")] = df.loc[:, df.columns.str.contains("age")].apply(standart_scaler)
 
 df.head()
+
+################################
+# Birleştirme (Join) İşlemleri
+################################
+import numpy as np
+import pandas as pd
+
+m = np.random.randint(1, 30, size=(5, 3))
+df1 = pd.DataFrame(m, columns=["var1", "var2", "var3"])
+df2 = df1 + 99
+
+pd.concat([df1, df2])
+
+pd.concat([df1, df2], ignore_index=True)
+
+#####################
+# Merge ile Birleştirme İşlemleri
+#####################
+
+df1 = pd.DataFrame({'employees': ['john', 'dennis', 'mark', 'maria'],
+                    'group': ["accounting", "engineering", "engineering", "hr"]})
+
+df2 = pd.DataFrame({'employees': ['mark', 'john', 'dennis', 'maria'],
+                    'start_date': [2010, 2009, 2014, 2019]})
+
+pd.merge(df1, df2)
+pd.merge(df1, df2, on="employees")
+
+# Amaç her çalışanın müdürünün bilgisine erişmek istiyoruz.
+df3 = pd.merge(df1, df2)
+
+df4 = pd.DataFrame({'group': ['accounting', 'engineering', 'hr'],
+                    'manager': ["Caner", "Mustafa", "Berkcan"]})
+
+pd.merge(df3,df4)
