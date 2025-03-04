3642b5f7feffc21f22fce68d5e7d519c74f9859b
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-03-05 00:06:32 +0300

    feat: Added initial scripts for general data overview and exploration

---
+++ V2/pythonProject/5-DataAnalysisWithPython/advanced_functional_eda.py
@@ -0,0 +1,53 @@
+#########################################################################
+# GELIŞMIŞ FONKSIYONEL KEŞIFÇI VERI ANALIZI (ADVANCED FUNCTIONAL EDA)
+#########################################################################
+# 1. Genel Resim
+# 2. Kategorik Değişken Analizi (Analysis of Categorical Variables)
+# 3. Sayısal Değişken Analizi (Analysis of Numerical Variables)
+# 4. Hedef Değişken Analizi (Analysis of Target Variable)
+# 5. Korelasyon Analizi (Analysis of Correlation)
+
+
+#############################################
+# 1. Genel Resim
+#############################################
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 500)
+df = sns.load_dataset("titanic")
+df.head()
+df.tail()
+df.shape
+df.info()
+df.columns
+df.index
+df.describe().T
+df.isnull().values.any()
+df.isnull().sum()
+
+
+def check_df(dataframe, head=5):
+    print("############################ SHAPE ############################")
+    print(dataframe.shape)
+    print("############################ TYPES ############################")
+    print(dataframe.dtypes)
+    print("############################ HEAD ############################")
+    print(dataframe.head(head))
+    print("############################ TAIL ############################")
+    print(dataframe.tail(head))
+    print("############################ NA ############################")
+    print(dataframe.isnull().sum())
+    print("############################ QUANTILES ############################")
+    print(dataframe.describe([0, 0.05, 0.50, 0.95, 0.99, 1]).T)
+
+
+check_df(df)
+
+
+df = sns.load_dataset("flights")
+
+check_df(df)
