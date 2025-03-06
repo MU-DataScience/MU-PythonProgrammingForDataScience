dc71487718b7a92d528f8d1e3cdb04bdc3066b58
MUSA UYUMAZ <musa.uyumaz73@gmail.com>
2025-03-06 23:25:49 +0300

    feat: Enhance correlation analysis framework and reporting

--- V2/pythonProject/5-DataAnalysisWithPython/advanced_functional_eda.py
+++ V2/pythonProject/5-DataAnalysisWithPython/advanced_functional_eda.py
@@ -446,3 +446,67 @@ target_summary_with_num(df, "survived", "age")
 
 for col in num_cols:
     target_summary_with_num(df, "survived", col)
+
+
+#################################################
+# 5. Korelasyon Analizi (Analysis of Correlation)
+#################################################
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 500)
+df = pd.read_csv("5-DataAnalysisWithPython/datasets/breast_cancer.csv")
+df = df.iloc[:, 1:-1]
+df.head()
+
+num_cols = [col for col in df.columns if df[col].dtypes in ["float", "int"]]
+
+corr = df[num_cols].corr()
+
+sns.set(rc={'figure.figsize': (12, 12)})
+sns.heatmap(corr, cmap="RdBu")
+plt.show()
+
+#######################################
+# Yüksek Korelasyonlu Değişkenlerin Silinmesi
+#######################################
+
+cor_matrix = df[num_cols].corr().abs()
+
+upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool_))
+drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > 0.90)]
+cor_matrix[drop_list]
+df.drop(cor_matrix[drop_list], axis=1)
+
+
+def high_correlated_cols(dataframe, plot=False, corr_th=0.90):
+    corr = dataframe.corr()
+    cor_matrix = corr.abs()
+    upper_triangle_matrix = cor_matrix.where(np.triu(np.ones(cor_matrix.shape), k=1).astype(np.bool_))
+    drop_list = [col for col in upper_triangle_matrix.columns if any(upper_triangle_matrix[col] > corr_th)]
+    if plot:
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.set(rc={"figure.figsize": (15, 15)})
+        sns.heatmap(corr, cmap="RdBu")
+        plt.show()
+    return drop_list
+
+
+high_correlated_cols(df[num_cols])
+drop_list = high_correlated_cols(df[num_cols], plot=True)
+high_correlated_cols(df[num_cols].drop(drop_list, axis=1), plot=True)
+
+# Yaklaşık 600 mb'lik 300'den fazla değişkenin olduğu bir veri setinde deneyelim.
+# https://www.kaggle.com/c/ieee-fraud-detection/data?select=train_transaction.csv
+
+df = pd.read_csv("5-DataAnalysisWithPython/datasets/application_train.csv")
+len(df.columns)
+df.head()
+
+drop_list = high_correlated_cols(df, plot=False)
+
+len(df.drop(drop_list, axis=1).columns)
